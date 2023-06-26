06648a4ee314ab668f308a058e04751087b061a1
Chrismaganga <chrismaganga3@gmail.com>
2023-06-27 00:11:08 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <button className="bg-red-500">
+           Save changes
+      </button>
+    </>
+  )
+}
+
+export default App
